094b2268260fe8b37a8d6faefc3ba11c93f5d96f
Aaron Ahmadi <aaron.k.ahmadi@gmail.com>
2020-07-18 23:56:59 +0200

    Students can only view the class they are in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Studentclass;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $studentclasses = Studentclass::all();
+
+        return view('home', compact('studentclasses'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,27 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">
+                    @can('manage-users')
+                    Redigera Användare
+                    @endcan
+
+                    @can('view-only')
+                    Mina sidor
+                    @endcan
+                </div>
 
                 <div class="card-body">
+                    @can('manage-users')
                     <a href="/studentclasses/create" class="btn btn-dark">Skapa en klass</a>
                     <a href="/students/create" class="btn btn-dark">Lägg till en student</a>
+                    @endcan
+
+                    @can('view-only')
+
+                    <a href="/studentclasses" class="btn btn-dark">Se mina klasser</a>
+
+                    @endcan
                 </div>
             </div>
         </div>
